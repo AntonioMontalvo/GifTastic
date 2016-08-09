488be6f835104fdb3ec800bc80ed74579157f1ea
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-08 17:53:32 -0700

    total functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,30 @@
 // rating - limit results to those rated (y,g, pg, pg-13 or r).
 //Sample: http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC   
 
-	var topics = ['matrix', 'notebook', 'mister', 'lion'];
+	var topics = ['akira', 'disney', 'studio ghibli', 'nemo', 'superman', 'thor', 'tow-mater'];
+
+	$('#addTopic').on('click', function(){		
+		var topic = $('#gif-input').val().trim();// store the input from the textbox
+		topics.push(topic);//add topic to topics array
+
+		renderButtons();
+	
+		return false;
+	});
+
+	function renderButtons(){ 
+		$('#topicsButtons').empty();//delete the movies shown
+
+		for (var i = 0; i < topics.length; i++){
+			// Then dynamicaly generates buttons for each movie in the array
+		    var newButtons = $('<button>') // This creates a button
+		    newButtons.addClass('gifs'); // Add a class 
+		    newButtons.attr('data-name', topics[i]); // Add a data-attribute
+		    newButtons.text(topics[i]); // Provided the initial button text
+		    $('#topicsButtons').append(newButtons); // Add the button to the HTML
+
+		}
+	}
 
 	function displayGifs() {
 		$('#gifScreens').empty();
@@ -29,8 +52,11 @@
 
                     var p = $('<p>').text(results[i].rating);
                     var topicImage = $('<img>');
-                    topicImage.attr('src', results[i].images.fixed_height.url);
-                    topicImage.attr('class', 'gifs')
+                    topicImage.attr('src', results[i].images.fixed_height_still.url);
+                    topicImage.attr('class', 'topicSet');
+                    topicImage.attr('data-animate', results[i].images.fixed_height.url);
+                    topicImage.attr('data-still', results[i].images.fixed_height_still.url);
+                    topicImage.attr('data-state', 'still');
                     topicDiv.append(p);
                     topicDiv.append(topicImage);
 
@@ -38,33 +64,32 @@
                     }
              });
     };
-
-
-	$('#addTopic').on('click', function(){		
-		var topic = $('#gif-input').val().trim();// store the input from the textbox
-		topics.push(topic);//add topic to topics array
-
-		renderButtons();
-	
-		return false;
-	});
-
-
-	function renderButtons(){ 
-		$('#topicsButtons').empty();//delete the movies shown
-
-		for (var i = 0; i < topics.length; i++){
-			// Then dynamicaly generates buttons for each movie in the array
-		    var newButtons = $('<button>') // This creates a button
-		    newButtons.addClass('gifs'); // Add a class 
-		    newButtons.attr('data-name', topics[i]); // Add a data-attribute
-		    newButtons.text(topics[i]); // Provided the initial button text
-		    $('#topicsButtons').append(newButtons); // Add the button to the HTML
-
-		}
-	}
-
 	$(document).on('click', '.gifs', displayGifs);
+
+
+
+function startStopGif(){
+	        var state = $(this).attr('data-state'); 
+	   
+            if ( state == 'still'){
+                $(this).attr('src', $(this).data('animate'));
+                $(this).attr('data-state', 'animate');
+            }else{
+                $(this).attr('src', $(this).data('still'));
+                $(this).attr('data-state', 'still');
+            }
+        
+	    };
+
+	 $(document).on('click', '.topicSet', startStopGif);   
+
+
+
+
+
+
+
+
 
 
 
